b1d9929c61b363d0551cb46a517737d15c3b9950
Laura Galbraith <laura_b_galbraith@msn.com>
2021-11-30 23:06:51 -0800

    Create files for Day 1 of 2021

---
+++ day1/puzzle1/main.cpp
@@ -0,0 +1,4 @@
+// main.cpp: Laura Galbraith
+// Description: solver for Puzzle 1 of Day 1 of The Advent Of Code 2021
+// See: https://adventofcode.com/2021
+// How many measurements are larger than the previous measurement?
