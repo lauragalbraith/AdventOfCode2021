93e575bb864aef3bca704783f91cecf6005a4f5c
Laura Galbraith <laura_b_galbraith@msn.com>
2021-12-01 12:08:56 -0800

    Implement more efficient version of Day1 Puzzle2

--- day1/puzzle2/main.cpp
+++ day1/puzzle2/main.cpp
@@ -13,7 +13,8 @@ using namespace std;
 const string kInputFileName = "../input.txt";
 const int kWindowSize = 3;
 
-// O(TODO once done) where n is the length of input
+// O(m*n) where n is the length of input, and m is kWindowSize
+// Since kWindowSize=3 is small, it is trivial to re-compute each sum it every time. 
 int BruteForce(const vector<int> input) {
   if (input.size() <= kWindowSize) {
     return 0;
@@ -21,7 +22,46 @@ int BruteForce(const vector<int> input) {
 
   int increases = 0;
   for (int prev = 0, curr = 1; curr < input.size() - kWindowSize + 1; ++prev, ++curr) {
-    cout << input[prev] << " " << input[curr] << endl;
+    // Compute the sums of each window
+    int prev_sum = 0, curr_sum = 0;
+    // Compute the sum in the window using the constant variable rather than having to assume its value is 3 here
+    for (int i = prev; i < prev + kWindowSize; ++i) {
+      prev_sum += input[i];
+    }
+    for (int i = curr; i < curr + kWindowSize; ++i) {
+      curr_sum += input[i];
+    }
+    
+    // Evaluate if it increased
+    if (curr_sum > prev_sum) { 
+      ++increases;
+    }
+  }
+  return increases;
+}
+
+// O(n) where n is the length of input; m (where m is window_size) elements are looped through, doing constant work, and then n-m elements are looped through, doing constant work
+// If the window size became large, we should cache each sum to only compute it once, or have a sliding computation which subtracts the dropped window element and adds the new window element
+int SlidingSums(const vector<int> input, const int window_size) {
+  if (input.size() <= window_size) {
+    return 0;
+  }
+
+  int increases = 0;
+  int prev_sum = 0, curr_sum = 0;
+  // Compute first sum
+  for (int i = 0; i < window_size; ++i) {
+    prev_sum += input[i];
+  }
+
+  // Compute the new sum in a rolling fashion
+  for (int prev_idx = 0, incoming_win_idx = window_size; incoming_win_idx < input.size(); ++prev_idx, ++incoming_win_idx) {
+    curr_sum = prev_sum - input[prev_idx] + input[incoming_win_idx];
+    if (curr_sum > prev_sum) {
+      ++increases;
+    }
+
+    prev_sum = curr_sum;
   }
   return increases;
 }
@@ -43,7 +83,7 @@ int main() {
   }
 
   // Process input
-  int answer = BruteForce(input);
+  int answer = SlidingSums(input, kWindowSize);
   cout << answer << endl;
 
   return 0;
