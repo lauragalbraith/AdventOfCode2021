e18b02b645e12ae94ba8bad388bdb5303b153d45
Laura Galbraith <laura_b_galbraith@msn.com>
2021-12-03 12:37:10 -0800

    Completed Day3 Puzzle1; initial implementation of Puzzle2

--- day3/puzzle1/main.cpp
+++ day3/puzzle1/main.cpp
@@ -28,13 +28,43 @@ const vector<string> ReadLinesFromFile(const string file_name) {
   return lines;
 }
 
-const vector<bitset> 
-
 int main() {
   // read in file, as vector of strings
   vector<string> input = ReadLinesFromFile("../input.txt");
-  // investigate C++ types for binary numbers, because I want to examine bit by bit
 
-  // Are there any easy binary ops which can tell me the most common bit across an array of binary numbers? AND / OR / XOR / NOR - probably not, because binary operations take in two values, and if there was a whole pile of 0s vs one 0, it should interact with a single 1 the same... if we were to order them, maybe, but then we'd have to reorder them for every bit position
+  // parse lines from file as binary values
+  vector<bitset<12>> values;
+  for (vector<string>::const_iterator i = input.begin(); i != input.end(); ++i) {
+    bitset<12> binary_value(*i);
+    values.push_back(binary_value);
+  }
+
+  bitset<12> gamma_rate; // Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position
+  bitset<12> epsilon_rate; // The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used
+  for (int i = 0; i < 12; ++i) {
+    int ones_count = 0;
+    int zeros_count = 0;
+
+    // Determine how many of each bit value is seen
+    // Are there any easy binary ops which can tell me the most common bit across an array of binary numbers? AND / OR / XOR / NOR - probably not, because binary operations take in two values, and if there was a whole pile of 0s vs one 0, it should interact with a single 1 the same... if we were to order them, maybe, but then we'd have to reorder them for every bit position
+    for (vector<bitset<12>>::const_iterator j = values.begin(); j != values.end(); ++j) {
+      bitset<12> value = *j;
+      if (value[i] == 1) {
+        ++ones_count;
+      }
+      else {
+        ++zeros_count;
+      }
+    }
+
+    gamma_rate[i] = 1 ? (ones_count > zeros_count) : 0; // if there are an equal number of 1s and 0s, 0 will be used (this is not specified in the puzzle rules, but has to be a choice somewhere)
+    epsilon_rate[i] = ~gamma_rate[i]; // since there are only 2 bit options, the most common bit is the opposite of the least common bit
+  }
+
+  cout << "Part 1:" << endl;
+  cout << "Final gamma rate is " << gamma_rate << " in binary and " << gamma_rate.to_ulong() << " in decimal" << endl;
+  cout << "Final epsilon rate is " << epsilon_rate << " in binary and " << epsilon_rate.to_ulong() << " in decimal" << endl;
+  cout << "Answer: " << gamma_rate.to_ulong() * epsilon_rate.to_ulong() << endl;
+
   return 0;
 }
--- day3/puzzle2/main.cpp
+++ day3/puzzle2/main.cpp
@@ -1,1 +1,86 @@
-// TODO
+// main.cpp: Laura Galbraith
+// Description: solver for Puzzle 2 of Day 2 of The Advent Of Code 2021
+// See: https://adventofcode.com/2021
+// What is the life support rating of the submarine?
+
+#include <iostream>
+#include <fstream>
+#include <bitset> // would use https://www.cplusplus.com/reference/vector/vector-bool/ if binary length was unknown
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+// O(n) where n is the number of lines in the file
+const vector<string> ReadLinesFromFile(const string file_name) {
+  ifstream f(file_name);
+  vector<string> lines;
+  if (f.is_open()) {
+    string line;
+    while (getline(f, line)) {
+      lines.push_back(line);
+    }
+  }
+  else {
+    cout << "File could not be opened" << endl;
+  }
+  return lines;
+}
+
+// O(TODO)
+bitset<12> CalculateOxGenRating(queue<bitset<12>> values) {
+  queue<bitset<12>>* initial_set = &values;
+  int bit_under_consideration = 11; // start with the "first" position out of 12
+  while (initial_set->size() > 1) {
+    queue<bitset<12>>* ones_set = new queue<bitset<12>>;
+    queue<bitset<12>>* zeros_set = new queue<bitset<12>>;
+
+    // Determine how many of each bit value is seen
+    for (int j = 0; j < initial_set->size()) {
+      bitset<12> value = initial_set->pop_front();
+      if (value[bit_under_consideration] == 1) {
+        ones_set->push_back(value);
+      }
+      else {
+        zeros_set->push_back(value);
+      }
+    }
+
+    delete(initial_set);
+    if (ones_set->size() > zeros_set->size()) {
+      initial_set = ones_set;
+      delete(zeros_set);
+    }
+    else {
+      initial_set = zeros_set;
+      delete(ones_set);
+    }
+  }
+
+  bitset<12> final_value = initial_set->pop_front();
+  delete(initial_set);
+
+  return final_value;
+}
+
+int main() {
+  // read in file, as vector of strings
+  vector<string> input = ReadLinesFromFile("../input.txt");
+
+  // parse lines from file as binary values
+  queue<bitset<12>> initial_set();
+  for (vector<string>::const_iterator i = input.begin(); i != input.end(); ++i) {
+    bitset<12> binary_value(*i);
+    initial_set.push_back(binary_value);
+  }
+
+  bitset<12> ox_gen_rating = CalculateOxGenRating(initial_set);
+
+  cout << "Part 2:" << endl;
+  cout << "Final oxygen generator rating is " << ox_gen_rating << " in binary and " << ox_gen_rating.to_ulong() << " in decimal" << endl;
+  cout << "Final CO2 scrubber rating is " << "TODO" << " in binary and " << "TODO" << " in decimal" << endl;
+  cout << "Answer: " << "TODO" << endl;
+
+  return 0;
+}
