8504d75476ac79243d6893aebcdf79c0d9e800a9
Laura Galbraith <laura_b_galbraith@msn.com>
2021-12-04 10:29:04 -0800

    Set up files for Day 4 puzzles

---
+++ day4/main.cpp
@@ -0,0 +1,4 @@
+// main.cpp: Laura Galbraith
+// Description: solver for Puzzles 1 and 2 of Day 4 of The Advent Of Code 2021
+// See: https://adventofcode.com/2021
+// What will your final score be if you choose the first winning bingo board?
