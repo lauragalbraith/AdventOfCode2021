f544549a1e031dd7aff8a83f27ffd3f571bf638c
Laura Galbraith <laura_b_galbraith@msn.com>
2021-12-03 16:10:25 -0800

    Confirmed no memory leaks in Puzzle 2 of Day 3

--- day3/puzzle2/main.cpp
+++ day3/puzzle2/main.cpp
@@ -28,24 +28,18 @@ const vector<string> ReadLinesFromFile(const string file_name) {
   return lines;
 }
 
-// O(TODO)
+// O(x*n) runtime where x is the number of bit positions in the data (in our case, 12), and n is the initial number of binary values (which never decreases in the worst case)
+// O(x*n) memory used, because the whole list is only stored in one place in each iteration, though it is duplicated in the intiial memory allocation
 bitset<12> CalculateOxGenRating(queue<bitset<12>> values) {
   queue<bitset<12>>* initial_set = new queue<bitset<12>>(values); // use "new" rather than just taking the pointer of "values" so it is initialized on the heap
-  cout << "entered CalculateOxGenRating with " << initial_set->size() << " elements" << endl;
   int bit_under_consideration = 11; // start with the "first" position out of 12
   while (initial_set->size() > 1 && bit_under_consideration >= 0) {
-    // cout << "in while loop with " << endl;
-    // cout << initial_set->size() << endl;
-    // cout << bit_under_consideration << endl;
     // Determine how many of each bit value is seen
     queue<bitset<12>>* ones_set = new queue<bitset<12>>;
     queue<bitset<12>>* zeros_set = new queue<bitset<12>>;
 
     int elems_left = initial_set->size();
     for (int i = 0; i < elems_left; ++i) {
-      // cout << "in for loop with " << endl;
-      // cout << i << endl;
-      // cout << initial_set->size() << endl;
       bitset<12> value = initial_set->front();
       initial_set->pop();
       if (value[bit_under_consideration] == 1) {
@@ -54,27 +48,16 @@ bitset<12> CalculateOxGenRating(queue<bitset<12>> values) {
       else {
         zeros_set->push(value);
       }
-
-      // cout << "end of for loop" << endl;
     }
-    // cout << "outside of for loop with" << endl;
-    // cout << initial_set->size() << endl;
-    // cout << ones_set->size() << endl;
-    // cout << zeros_set->size() << endl;
 
     delete initial_set;
-    // cout << "after cleaning initial_set" << endl;
     if (ones_set->size() >= zeros_set->size()) { // now specified: If 0 and 1 are equally common, keep values with a 1 in the position being considered
-      // cout << "before cleaning zeros_set" << endl;
       initial_set = ones_set;
       delete zeros_set;
-      // cout << "after cleaning zeros_set" << endl; // TODO remove printout comments
     }
     else {
-      // cout << "before cleaning ones_set" << endl;
       initial_set = zeros_set;
       delete ones_set;
-      // cout << "after cleaning ones_set" << endl;
     }
 
     --bit_under_consideration;
@@ -91,24 +74,18 @@ bitset<12> CalculateOxGenRating(queue<bitset<12>> values) {
   return final_value;
 }
 
-// O(TODO)
-bitset<12> CalculateCO2ScrubberRating(queue<bitset<12>> values) { // TODO run program thru valgrind/asan/msan/tsan to see if I missed any memory allocations
+// O(x*n) runtime where x is the number of bit positions in the data (in our case, 12), and n is the initial number of binary values (which never decreases in the worst case)
+// O(x*n) memory used, because the whole list is only stored in one place in each iteration, though it is duplicated in the intiial memory allocation
+bitset<12> CalculateCO2ScrubberRating(queue<bitset<12>> values) {
   queue<bitset<12>>* initial_set = new queue<bitset<12>>(values); // use "new" rather than just taking the pointer of "values" so it is initialized on the heap
-  cout << "entered CalculateCO2ScrubberRating with " << initial_set->size() << " elements" << endl;
   int bit_under_consideration = 11; // start with the "first" position out of 12
   while (initial_set->size() > 1 && bit_under_consideration >= 0) {
-    // cout << "in while loop with " << endl;
-    // cout << initial_set->size() << endl;
-    // cout << bit_under_consideration << endl;
     // Determine how many of each bit value is seen
     queue<bitset<12>>* ones_set = new queue<bitset<12>>;
     queue<bitset<12>>* zeros_set = new queue<bitset<12>>;
 
     int elems_left = initial_set->size();
     for (int i = 0; i < elems_left; ++i) {
-      // cout << "in for loop with " << endl;
-      // cout << i << endl;
-      // cout << initial_set->size() << endl;
       bitset<12> value = initial_set->front();
       initial_set->pop();
       if (value[bit_under_consideration] == 1) {
@@ -117,28 +94,17 @@ bitset<12> CalculateCO2ScrubberRating(queue<bitset<12>> values) { // TODO run pr
       else {
         zeros_set->push(value);
       }
-
-      // cout << "end of for loop" << endl;
     }
-    // cout << "outside of for loop with" << endl;
-    // cout << initial_set->size() << endl;
-    // cout << ones_set->size() << endl;
-    // cout << zeros_set->size() << endl;
 
     delete initial_set;
-    // cout << "after cleaning initial_set" << endl;
     // now specified: If 0 and 1 are equally common, keep values with a 0 in the position being considered
     if (zeros_set->size() <= ones_set->size()) { // TODO if changing this single line from > to < gets me the right answer, pass a function into this function to be > or < to get rid of duplicate code
-      // cout << "before cleaning ones_set" << endl;
       initial_set = zeros_set;
       delete ones_set;
-      // cout << "after cleaning ones_set" << endl;
     }
     else {
-      // cout << "before cleaning zeros_set" << endl;
       initial_set = ones_set;
       delete zeros_set;
-      // cout << "after cleaning zeros_set" << endl;
     }
 
     --bit_under_consideration;
@@ -173,6 +139,17 @@ int main() {
   cout << "Final oxygen generator rating is " << ox_gen_rating << " in binary and " << ox_gen_rating.to_ulong() << " in decimal" << endl;
   cout << "Final CO2 scrubber rating is " << co2_scrubber_rating << " in binary and " << co2_scrubber_rating.to_ulong() << " in decimal" << endl;
   cout << "Answer: " << ox_gen_rating.to_ulong() * co2_scrubber_rating.to_ulong() << endl;
+
+  /*
+   C++ Language Options
+           -fabi-version=n  -fno-access-control -faligned-new=n  -fargs-in-order=n  -fchar8_t  -fcheck-new -fconstexpr-depth=n  -fconstexpr-loop-limit=n -fconstexpr-ops-limit=n -fno-elide-constructors
+           -fno-enforce-eh-specs -fno-gnu-keywords -fno-implicit-templates -fno-implicit-inline-templates -fno-implement-inlines  -fms-extensions -fnew-inheriting-ctors -fnew-ttp-matching -fno-nonansi-builtins
+           -fnothrow-opt  -fno-operator-names -fno-optional-diags  -fpermissive -fno-pretty-templates -frepo  -fno-rtti  -fsized-deallocation -ftemplate-backtrace-limit=n -ftemplate-depth=n
+           -fno-threadsafe-statics  -fuse-cxa-atexit -fno-weak  -nostdinc++ -fvisibility-inlines-hidden -fvisibility-ms-compat -fext-numeric-literals -Wabi=n  -Wabi-tag  -Wconversion-null  -Wctor-dtor-privacy
+           -Wdelete-non-virtual-dtor  -Wdeprecated-copy  -Wdeprecated-copy-dtor -Wliteral-suffix -Wmultiple-inheritance  -Wno-init-list-lifetime -Wnamespaces  -Wnarrowing -Wpessimizing-move  -Wredundant-move
+           -Wnoexcept  -Wnoexcept-type  -Wclass-memaccess -Wnon-virtual-dtor  -Wreorder  -Wregister -Weffc++  -Wstrict-null-sentinel  -Wtemplates -Wno-non-template-friend  -Wold-style-cast -Woverloaded-virtual
+           -Wno-pmf-conversions -Wno-class-conversion  -Wno-terminate -Wsign-promo  -Wvirtual-inheritance
+  */
 
   return 0;
 }
