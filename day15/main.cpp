b0f6d6e4b3d6dfffd6866ef7d2ae6af454a95c2d
Laura Galbraith <laura_b_galbraith@msn.com>
2021-12-14 21:31:32 -0800

    Ideas for Puzzle 1 for Day 15

--- day15/main.cpp
+++ day15/main.cpp
@@ -1,7 +1,7 @@
 // main.cpp: Laura Galbraith
 // Description: solver for Puzzles 1 and 2 of Day 15 of The Advent Of Code 2021
 // See: https://adventofcode.com/2021
-// Part 1: TODO
+// Part 1: What is the lowest total risk of any path from the top left to the bottom right?
 // Part 2: TODO
 
 #include "../util/fileutil.hpp" // ReadLinesFromFile
@@ -13,7 +13,6 @@
 using namespace std;
 
 int main() {
-
   pair<vector<string>, int> file_results = ReadLinesFromFile("day15/input.txt");
   if (file_results.second < 0) {
     cout << "Failed to read file" << endl;
@@ -26,6 +25,7 @@ int main() {
   }
 
   // Part 1:
+  // TODO IDEA: path-finding algorithm (probably Dijkstra's) where distance is jsut the value of the end node, and adjacent nodes just being up,down,left,right
 
   // Part 2:
   // TODO
