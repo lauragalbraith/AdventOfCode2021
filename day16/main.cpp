d2e9903fd1a1e6f585dca1b6ecf6fca5e5bb1f1d
Laura Galbraith <laura_b_galbraith@msn.com>
2021-12-19 12:03:28 -0800

    Solved Puzzle 1 of Day 16

--- day16/main.cpp
+++ day16/main.cpp
@@ -68,6 +68,7 @@ class Packet {
     Packet(const unsigned int& ver, const PacketType& type_id) : version(ver), type(type_id) {}
     unsigned int GetVersion() { return this->version; }
     PacketType GetType() { return this->type; }
+    virtual unsigned int SumPacketAndSubPacketVersions() { return 0; }
     virtual ~Packet() {} // must be made virtual so that we can delete any packet using the derived class's destructor
 };
 
@@ -79,14 +80,49 @@ class LiteralValuePacket : public Packet {
   public:
     LiteralValuePacket(const unsigned int& val, const unsigned int& ver) : Packet(ver,literal_val), value(val) {}
     unsigned int GetValue() { return this->value; }
+    unsigned int SumPacketAndSubPacketVersions() { return this->version; }
     ~LiteralValuePacket() {} // No internal memory usage to clear
-    static LiteralValuePacket* ParseLiteralValuePacket(const vector<bool>& input);
+    static pair<LiteralValuePacket*,vector<bool>::const_iterator> ParseLiteralValuePacket(
+      vector<bool>::const_iterator start,
+      const vector<bool>::const_iterator& end);
 };
 
-LiteralValuePacket* LiteralValuePacket::ParseLiteralValuePacket(const vector<bool>& input) {
-  // TODO consider making the return value any unused elements from the end of input
-  // TODO use new to create the returned pointers
-  return NULL;
+// ParseLiteralValuePacket will iterate over the bytes pointed to between the given iterators (most significant bit first) and return the represented literal value packet
+// The packet encodes a single binary number, padded with leading zeroes until its length is a multiple of four bits; each group of four bits is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit; ex. 110 (version) 100 (type ID) 10111 (0111 is the first bits of the value) 11110 (1110 is the next bits of the value) 00101 (0101 is the last bits of the value) 000 (just padding zeroes)
+pair<LiteralValuePacket*,vector<bool>::const_iterator> LiteralValuePacket::ParseLiteralValuePacket(
+  vector<bool>::const_iterator start,
+  const vector<bool>::const_iterator& end)
+{
+  vector<bool>::const_iterator current = start;
+
+  // Parse header
+  unsigned int version = ParseBytesAsInt(current, current+3);
+  current += 6; // skip type parsing, as only one value is for literal values
+  cout << "Version of literal value packet: " << version << endl;
+
+  // Despite the rules mentioning leading zeros (padding until length is 4x bits), this does not exist anywhere in the input
+
+  // Parse value...
+  vector<bool> value_bits;
+
+  // ...parse all groups but last
+  while (*current == 1) {
+    value_bits.insert(value_bits.end(),current+1, current+5);
+    current += 5;
+  }
+
+  // ...parse last group
+  value_bits.insert(value_bits.end(),current+1, current+5);
+  current += 5;
+
+  unsigned int value = ParseBytesAsInt(value_bits.begin(), value_bits.end());
+
+  // Form operator packet object
+  LiteralValuePacket* lv_p = new LiteralValuePacket(value, version);
+
+  // Despite the rules mentioning "0 bits at the end are extra due to the hexadecimal representation and should be ignored", there are no such examples of this, where the trailing 0s could be considered at the end of the outermost packet
+
+  return pair<LiteralValuePacket*,vector<bool>::const_iterator>(lv_p, current);
 }
 
 class OperatorPacket : public Packet {
@@ -100,11 +136,18 @@ class OperatorPacket : public Packet {
       }
     }
 
-    void AppendContainedPacket(Packet* p) { this->contained_packets.push_back(p); } // TODO remove this if my code can instead set all the contained packets in the object constructor
+    void AppendContainedPacket(Packet* p) { this->contained_packets.push_back(p); } // TODO FINALLY remove this if my code can instead set all the contained packets in the object constructor
     static pair<OperatorPacket*,vector<bool>::const_iterator> ParseOperatorPacket(
       vector<bool>::const_iterator current,
       const vector<bool>::const_iterator& end);
     vector<Packet*> GetContainedPackets() { return this->contained_packets; }
+    unsigned int SumPacketAndSubPacketVersions() {
+      unsigned int total = this->version;
+      for (auto p:this->contained_packets) {
+        total += p->SumPacketAndSubPacketVersions();
+      }
+      return total;
+    }
 
     ~OperatorPacket() {
       for (int i = 0; i < this->contained_packets.size(); ++i) {
@@ -115,7 +158,7 @@ class OperatorPacket : public Packet {
     }
 };
 
-// ParseOperatorPacket will iterate over the bytes pointed to between the given iterators (most significant bit first) and return the operator packet
+// ParseOperatorPacket will iterate over the bytes pointed to between the given iterators (most significant bit first) and return the represented operator packet
 // After the version/type ID, it has a length type ID bit (0: next 15 bits are the total length in bits of the sub-packets in the operator; 1: next 11 bits are the number of sub-packets immediately contained by the operator), then the sub-packets
 pair<OperatorPacket*,vector<bool>::const_iterator> OperatorPacket::ParseOperatorPacket(
   vector<bool>::const_iterator start,
@@ -129,7 +172,7 @@ pair<OperatorPacket*,vector<bool>::const_iterator> OperatorPacket::ParseOperator
   unsigned int type_id = ParseBytesAsInt(current, current+3);
   current += 3;
   PacketType type = static_cast<PacketType>(type_id);
-  cout << "Version: " << version << ", Type: " << type << endl;
+  cout << "Version of operator packet: " << version << ", Type: " << type << endl;
 
   // Parse length type ID
   bool length_type_id = *current;
@@ -139,6 +182,7 @@ pair<OperatorPacket*,vector<bool>::const_iterator> OperatorPacket::ParseOperator
   // Parse the length bits and sub-packets
   vector<Packet*> sub_packets;
   if (length_type_id == 0) { // 15 bits are the length in bits of the sub-packets
+    // Note: this length does not account for leading zeroes at the end of the operator packet
     unsigned int bit_length_sub = ParseBytesAsInt(current, current + 15);
     current += 15;
     cout << "Length in bits of the sub-packets: " << bit_length_sub << " compared to length until end: " << (end - current) << endl;
@@ -171,9 +215,7 @@ pair<OperatorPacket*,vector<bool>::const_iterator> OperatorPacket::ParseOperator
   // Form operator packet object
   OperatorPacket* op_p = new OperatorPacket(version, sub_packets); // TODO PART 2: if type matters beyond operator/literal, pass it in here
 
-  // Move through any remaining zero-padding before returning this packet and remaining bits, if any
-  // TODO HERE
-  // TODO update current to the next bit that we haven't processed
+  // Despite the rules mentioning trailing zero-padding in packets, none exists except in the outermost packet
 
   return pair<OperatorPacket*,vector<bool>::const_iterator>(op_p, current);
 }
@@ -183,23 +225,12 @@ pair<OperatorPacket*,vector<bool>::const_iterator> OperatorPacket::ParseOperator
 pair<Packet*,vector<bool>::const_iterator> ParseSinglePacket(vector<bool>::const_iterator start, const vector<bool>::const_iterator& limit) {
   // Parse packet type ID to determine which parsing function to call
   unsigned int type_id = ParseBytesAsInt(start+3, start+6);
-
-  Packet* p = NULL;
-  vector<bool>::const_iterator remaining;
   if (type_id == literal_val) {
-    // TODO NEXT 20
+    return LiteralValuePacket::ParseLiteralValuePacket(start, limit);
   }
   else {
-    OperatorPacket::ParseOperatorPacket(start, limit);
-    // TODO NEXT 5
+    return OperatorPacket::ParseOperatorPacket(start, limit);
   }
-
-  return pair<Packet*,vector<bool>::const_iterator>(p, limit);
-}
-
-unsigned int SumAllPacketVersions(const Packet* packet) {
-  // TODO NEXT 10 recursively call this on all contained packets - consider making it an object method on top of operator packet which will recursively call it for all of its contained packets
-  return 0;
 }
 
 int main() {
@@ -227,26 +258,19 @@ int main() {
   // Part 1:
   // Since we know that the whole BITS transmission is a single packet "which itself contains many other packets", it must be an operator type
   pair<OperatorPacket*,vector<bool>::const_iterator> packet_result = OperatorPacket::ParseOperatorPacket(input.begin(), input.end());
-  if (packet_result.second != input.end()) {
+
+  // Move thru trailing zeroes at the end of the outermost packet
+  vector<bool>::const_iterator current = packet_result.second;
+  while (current < input.end() && *current == 0) {
+    ++current;
+  }
+  if (current != input.end()) {
     cout << "Packet parsing did not continue to the end of the input; it was " << (input.end() - packet_result.second) << " bits off" << endl;
     return -1;
   }
 
   Packet* outer_packet = packet_result.first;
-  unsigned int version_total = SumAllPacketVersions(outer_packet);
-  cout << "Part 1 answer: " << version_total << endl;
-
-  // TODO move my notes to comments over functions, etc.
-  // The entire sequence is a single packet which contains many other packets
-  // Disregard any extra 0 bits at the end of the outermost packet
-
-  // Packet header: 3 bits version, 3 bits type ID (both binary numbers, i.e. 100=4)
-  // Type ID 4: literal value; the rest of the packet encodes a single binary number, padded with leading zeroes until its length is a multiple of four bits; each group of four bits is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit; ex. 110 (version) 100 (type ID) 10111 (0111 is the first bits of the value) 11110 (1110 is the next bits of the value) 00101 (0101 is the last bits of the value) 000 (just padding zeroes)
-
-  
-
-  // helper function to decode a literal value from bits, and return any unused bits after the 0-padding
-  // helper function to decode an operator value from bits, which may recursively call itself or call the literal-decoder; they can return objects representing a value or an operator, which can each contain a vector of Packets inside them
+  cout << "Part 1 answer: " << outer_packet->SumPacketAndSubPacketVersions() << endl;
 
   // Part 2:
   // TODO
