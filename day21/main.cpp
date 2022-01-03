4ba846c9da0163d7f9f616254fab20aeedfb7aad
Laura Galbraith <laura_b_galbraith@msn.com>
2022-01-02 17:33:33 -0800

    Day 21: Solved Puzzle 2

--- day21/main.cpp
+++ day21/main.cpp
@@ -12,8 +12,11 @@
 #include <regex>
 #include <queue>
 #include <map>
+#include <cmath>
 
 using namespace std;
+
+const int NUM_ROLLS = 3;
 
 class Die {
   protected:
@@ -73,6 +76,7 @@ class DiracDie : public Die {
       }
     }
 
+    // returns the roll options in ascending order
     const vector<int> Roll() {
       Die::Roll();
       return this->rolls;
@@ -88,23 +92,21 @@ class Player {
     const int min_win_score;
     int score;
 
+    void MoveAndUpdateScore(const int movement);
+
   public:
     Player(const int player_num, const int starting_space, const int win_min):
       number(player_num),
       min_win_score(win_min)
     {
-      // cout << "regular constructor on " << this << ": num " << this->number << ", min win score " << this->min_win_score << endl;
       this->space = starting_space;
       this->score = 0;
-      // TODO FINALLY remove debugging statements
     }
 
     Player(const Player& other): // copy constructor
       number(other.number),
       min_win_score(other.min_win_score)
     {
-      // cout << "copy constructor on " << this << ": num " << this->number << ", min win score " << this->min_win_score << endl;
-
       this->space = other.space;
       this->score = other.score;
     }
@@ -112,71 +114,101 @@ class Player {
     vector<Player*> TakeTurn(Die* die);
     const int GetPlayerNumber() const { return this->number; }
     const int GetScore() const { return this->score; }
+    const int GetSpace() const { return this->space; }
     const bool HasWon() const {
-      // cout << "HasWon has a score of " << this->score << " and a min win score of " << this->min_win_score << endl;
       return this->score >= this->min_win_score; 
+    }
+
+    // returns based on number, score, and space on board
+    // Because we want players with the smallest scores to be handled first (because scores cannot decrease), that will be the strongest factor to consider, then potential to increase score
+    bool operator<(const Player& other) const { // necessary to implement for to use Player type as key in map
+      if (this->score < other.score) {
+        return true;
+      }
+      else if (this->score == other.score) {
+        if (this->space < other.space) {
+          return true;
+        }
+        else if (this->space == other.space) {
+          if (this->number < other.number) {
+            return true;
+          }
+        }
+      }
+
+      return false;
     }
 
     ~Player() {}
 };
 
+// defining the << operator function to ease printing
+ostream& operator<<(ostream& os, const Player& p) {
+  os << "Player " << p.GetPlayerNumber() << " has score " << p.GetScore() << "; is on space " << p.GetSpace();
+  return os;
+}
+
 // TakeTurn will return the resulting quantum players from the turn's actions
 vector<Player*> Player::TakeTurn(Die* die) {
-  queue<pair<Player*,vector<int>>> player_rolls;
-  player_rolls.push(pair<Player*,vector<int>>(this, vector<int>()));
+  // Determine what the movement options for the player are
+  vector<double> all_movements;
+  vector<vector<int>> roll_options;
+  int option_count = 1;
+  for (int rolls = 1; rolls <= NUM_ROLLS; ++rolls) {
+    roll_options.push_back(die->Roll());
+    option_count *= roll_options[roll_options.size()-1].size();
+  }
+  all_movements.resize(option_count);
 
+  // Each movement option is a combination of roll options, where the movement's identifier (mov_idx) is equal to o0 * 3^2 + o1 * 3^1 + o2 * 3^0 where o0 is the *index* of the roll option chosen for the 3rd roll (example where 3 is the number of options per roll, and the exponents follow the range of indexes into the rolls (i.e. 0-2 for 3 rolls))
+  // NOTE: this would not work if we wanted to split universes with some identical roll options (ex. 1,1,2)
+  for (double mov_idx = 0; mov_idx < all_movements.size(); ++mov_idx) {
+    double movement = 0;
+    double remaining_mov_idx = mov_idx;
+
+    for (double roll_idx = NUM_ROLLS-1; roll_idx >= 0; --roll_idx) {
+      for (double option_idx = roll_options[roll_idx].size()-1; option_idx >= 0; --option_idx) {
+        double potential_option = option_idx * pow(roll_options[roll_idx].size(), roll_idx);
+        if (remaining_mov_idx - potential_option >= 0) {
+          movement += roll_options[roll_idx][option_idx];
+          remaining_mov_idx -= potential_option;
+          break; // stop considering options; move to next roll
+        }
+      }
+    }
+
+    all_movements[mov_idx] = movement;
+  }
+
+  // Create a new player universe out of any >1 movement options
   vector<Player*> resulting_players;
-
-  // Perform rolls in an expanding fashion
-  while (!player_rolls.empty()) {
-    pair<Player*,vector<int>> p = player_rolls.front();
-    player_rolls.pop();
-    Player* current_player = p.first;
-    vector<int> rolls_so_far = p.second;
-
-    // cout << current_player << " num " << current_player->GetPlayerNumber() << " has rolled " << rolls_so_far.size() << " times so far" << endl;
-
-    // Check if all rolls for this player are done
-    if (rolls_so_far.size() == 3) {
-      int movement = rolls_so_far[0] + rolls_so_far[1] + rolls_so_far[2];
-
-      // determine which space the player lands on
-      int space_value = (current_player->space + movement) % 10;
-      current_player->space = space_value == 0 ? 10 : space_value;
-
-      // update score according to space
-      current_player->score += current_player->space;
-      // cout << current_player << " now has a score of " << current_player->score << endl;
-
-      // include player in final result
-      resulting_players.push_back(current_player);
-
-      continue; // handle the next player in the queue
+  resulting_players.resize(all_movements.size());
+  for (double m = 0; m < all_movements.size(); ++m) {
+    Player* current_player;
+    if (m == 0) {
+      current_player = this;
     }
-
-    // Roll again for this player, potentially creating more player universes
-    vector<int> quantum_rolls = die->Roll();
-    for (int r = 0; r < quantum_rolls.size(); ++r) {
-      // Add on this quantum roll to the rolls so far
-      vector<int> total_rolls = vector<int>(rolls_so_far.begin(), rolls_so_far.end());
-      total_rolls.push_back(quantum_rolls[r]);
-
-      // Clone player for the new universe
-      Player* player;
-      if (r == 0) {
-        player = current_player; // save space by keeping the original player record in use
-      }
-      else {
-        // cout << "Calling copy constructor: " << current_player << ": num " << current_player->GetPlayerNumber() << " and min win score: " << current_player->min_win_score << endl;
-        player = new Player(*current_player);
-      }
-
-      // Add player to the back of the queue to keep rolling
-      player_rolls.push(pair<Player*,vector<int>>(player, total_rolls));
+    else {
+      current_player = new Player(*this);
     }
+    resulting_players[m] = current_player;
+  }
+
+  // once all player copies have been made, update the original's, and all others', scores
+  for (double m = 0; m < resulting_players.size(); ++m) {
+    resulting_players[m]->MoveAndUpdateScore((int)all_movements[m]);
   }
 
   return resulting_players;
+}
+
+void Player::MoveAndUpdateScore(const int movement) {
+  // determine which space the player lands on
+  int space_value = (this->space + movement) % 10;
+  this->space = space_value == 0 ? 10 : space_value;
+
+  // update score according to space
+  this->score += this->space;
 }
 
 int main() {
@@ -231,7 +263,6 @@ int main() {
   }
 
   Player* losing_player = winning_player_idx == 0 ? practice_players[1] : practice_players[0];
-
   cout << "Part 1 answer: " << losing_player->GetScore() * det_die->GetTimesRolled() << endl;
 
   // Clean up memory
@@ -243,8 +274,8 @@ int main() {
 
   // Part 2:
 
-  // Parse file input
-  vector<Player*> dirac_players;
+  // Re-parse file input
+  vector<Player> dirac_players;
   for (auto line:file_results.first) {
     smatch m;
     regex_search(line, m, r);
@@ -253,8 +284,8 @@ int main() {
       return -1;
     }
 
-    Player* player = new Player(stoi(m[1]), stoi(m[2]), 21);
-    dirac_players.push_back(player);
+    Player p(stoi(m[1]), stoi(m[2]), 21);
+    dirac_players.push_back(p);
   }
 
   if (dirac_players.size() != 2) {
@@ -264,57 +295,78 @@ int main() {
 
   DiracDie* dirac_die = new DiracDie(3);
 
-  queue<pair<Player*,Player*>> dirac_player_pairs;
-  dirac_player_pairs.push(pair<Player*,Player*>(dirac_players[0], dirac_players[1]));
-
+  // Track the number of wins of each player
   map<int,unsigned long long int> wins;
-  wins[dirac_players[0]->GetPlayerNumber()] = 0;
-  wins[dirac_players[1]->GetPlayerNumber()] = 0;
+  wins[dirac_players[0].GetPlayerNumber()] = 0;
+  wins[dirac_players[1].GetPlayerNumber()] = 0;
 
-  while (!dirac_player_pairs.empty()) {
-    cout << "Universes: " << dirac_player_pairs.size() << endl;
+  // Since players with the same number, score, and occupied space on the board are equivalently-likely to win, track pairs of players at any two values the same
+  // The pair type's evaluation does not use our operator< with pointer types, so for simplicity, we will not use Player pointers
+  map<pair<Player,Player>, unsigned long long int> universes;
+  pair<Player,Player> starting(dirac_players[0], dirac_players[1]);
+  universes[starting] = 1;
 
-    pair<Player*,Player*> players = dirac_player_pairs.front();
-    dirac_player_pairs.pop();
+  while (universes.size() > 0) {
+    auto u = universes.begin();
 
-    Player* turn_player = players.first;
-    Player* next_player = players.second;
+    // Clear the universes from the record while processing
+    Player player1 = u->first.first;
+    Player player2 = u->first.second;
+    unsigned long long int universe_count = u->second;
 
-    // Check if this player's game has ended
-    if (next_player->HasWon()) {
-      // cout << next_player << " num " << next_player->GetPlayerNumber() << " has won with score of " << next_player->GetScore() << endl;
+    universes.erase(u);
 
-      wins[next_player->GetPlayerNumber()] += 1;
-      delete turn_player;
-      delete next_player;
-      continue;
-    }
-    else if (turn_player->HasWon()) {
-      // cout << turn_player << " num " << turn_player->GetPlayerNumber() << " has won with score of " << turn_player->GetScore() << endl;
-
-      wins[turn_player->GetPlayerNumber()] += 1;
-      delete turn_player;
-      delete next_player;
+    // Since wins must be checked before the universe is expanded next, check only after universes have been expanded from previous player's turn
+    // Before the 1st player's turn, check 2nd player's wins now
+    if (player2.HasWon()) {
+      wins[player2.GetPlayerNumber()] += universe_count;
       continue;
     }
 
-    // Create new player universes based on this player's turn
-    vector<Player*> resulting_turn_players = turn_player->TakeTurn(dirac_die);
+    // Take the 1st player's turn
+    vector<Player*> resulting_player1s = player1.TakeTurn(dirac_die);
 
-    // Add all player pairs back to the queue, in their next turn order
-    for (int p = 0; p < resulting_turn_players.size(); ++p) {
-      // cout << "After their turn, " << p << ", num " << resulting_turn_players[p]->GetPlayerNumber() << " now has a score of " << resulting_turn_players[p]->GetScore() << endl;
+    // Before the 2nd player's turn, check if 1st player has won
+    bool index0_still_stack_pointer = true;
+    for (int i = resulting_player1s.size()-1; i >= 0; --i) { // move backwards so we don't skip any values while erasing
+      if (resulting_player1s[i]->HasWon()) {
+        wins[resulting_player1s[i]->GetPlayerNumber()] += universe_count;
 
-      if (p == 0) { // save a little space by keeping the original next player pointer
-        dirac_player_pairs.push(pair<Player*,Player*>(next_player, resulting_turn_players[p]));
+        // If the first player1 in the list is no longer on the stack, we will need to delete it later
+        if (i == 0) {
+          index0_still_stack_pointer = false;
+        }
+        else {
+          delete resulting_player1s[i];
+        }
+
+        resulting_player1s.erase(resulting_player1s.begin()+i);
       }
-      else {
-        dirac_player_pairs.push(pair<Player*,Player*>(new Player(*next_player), resulting_turn_players[p]));
+    }
+
+    // Take the 2nd player's turn
+    vector<Player*> resulting_player2s = player2.TakeTurn(dirac_die);
+
+    // Combine the resulting players' universes to save in the map
+    for (auto p1:resulting_player1s) {
+      for (auto p2:resulting_player2s) {
+        pair<Player,Player> new_universe(*p1,*p2);
+        if (universes.find(new_universe) == universes.end()) {
+          universes[new_universe] = 0;
+        }
+        universes[new_universe] += universe_count;
       }
+    }
+
+    // Clear memory used in resulting player pointers (skip index 0 only if it's a stack pointer)
+    for (int i = index0_still_stack_pointer ? 1 : 0; i < resulting_player1s.size(); ++i) {
+      delete resulting_player1s[i];
+    }
+    for (int i = 1; i < resulting_player2s.size(); ++i) {
+      delete resulting_player2s[i];
     }
   }
 
-  cout << "Checking win counts" << endl;
   unsigned long long int max_win_count = 0;
   for (auto c:wins) {
     if (c.second > max_win_count) {
@@ -322,13 +374,9 @@ int main() {
     }
   }
 
-  cout << "Part 2 answer: " << max_win_count << endl; // TODO HERE process is killed before it finishes, which means I'm probably using too much memory; is there a way I could process each 27 universes until completion? I though maybe do player 1, then player 2, then store in a map their two scores/positions, which has a small defined number of values, MAPPED TO the number of universes with that player pair score/position state; Die method to return a vector of possible roll totals (ex. [3,4,4,4,...]); then for each of those movement numbers,  simulate player 1's turn then player 2's turn IN A GIANT LOOP over the map tracking all possible player states & how many universes are in that state
+  cout << "Part 2 answer: " << max_win_count << endl;
 
   // Clean up memory
-  for (auto p:dirac_players) {
-    delete p;
-  }
-
   delete dirac_die;
 
   return 0;
